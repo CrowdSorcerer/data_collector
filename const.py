19219364591e8235f5de00c436dc852a9f250243
inryatt <cffonseca@ua.pt>
2022-05-08 11:06:17 +0100

    Removed duplicate files

--- const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the Crowdsourcerer integration."""
-
-DOMAIN = "data_collector"
-
-TIME_INTERVAL = 10  # In seconds
-BLACKLIST = []
-API_URL = "https://172.18.0.1:8080/api/ingest/data"
