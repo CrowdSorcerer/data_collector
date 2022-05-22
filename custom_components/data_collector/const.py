480a1911b622cf25da66f674cc42d522e3cf7b96
Inryatt <56603542+Inryatt@users.noreply.github.com>
2022-05-22 16:41:24 +0100

    Update const.py

--- custom_components/data_collector/const.py
+++ custom_components/data_collector/const.py
@@ -1,4 +1,5 @@
 """Constants for the Crowdsourcerer integration."""
+import logging
 
 DOMAIN = "data_collector"
 
@@ -6,3 +7,4 @@ TIME_INTERVAL = 10  # In seconds
 BLACKLIST = []
 API_URL = "https://smarthouse.av.it.pt/api/ingest/data"
 # API_URL = "https://172.18.0.1:8080/api/ingest/data"
+logger = logging.getLogger(__package__)
